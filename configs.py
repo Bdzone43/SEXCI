be13b1727a67d68301b5f624dd72c67d58aa5370
sexcie <158564724+Mareuxs@users.noreply.github.com>
2024-12-24 02:01:20 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,8 +2,8 @@ from os import path, getenv
 
 
 class Config:
-    API_ID = int(getenv('API_ID','23104928'))
-    API_HASH = getenv('API_HASH','af6425de587ed34c1b9071e26ccbf7e5')
-    BOT_TOKEN = getenv('BOT_TOKEN','7679006349:AAFM1bMmafYTQHXL-8SicF0gsxHUqdL1h_E')
+    API_ID = int(getenv('API_ID','xxxx'))
+    API_HASH = getenv('API_HASH','xxxx')
+    BOT_TOKEN = getenv('BOT_TOKEN','xxxx')
 
 config = Config()
