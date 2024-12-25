3da0c8500c5ca39f22cf94137888fc2f4c54c125
Bdzone43 <150906861+Bdzone43@users.noreply.github.com>
2024-12-25 09:51:13 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,8 +2,8 @@ from os import path, getenv
 
 
 class Config:
-    API_ID = int(getenv('API_ID','21603683'))
-    API_HASH = getenv('API_HASH','c531b0ec35017b19e043cf9f106ed95b')
+    API_ID = int(getenv('API_ID','28247126'))
+    API_HASH = getenv('API_HASH','cd2e50ee0d30c1a69fcbc45588b9471b')
     BOT_TOKEN = getenv('BOT_TOKEN','7622447051:AAErToQUO6DsVLoJlbFkiGFQFiBHEeZqVNk')
 
 config = Config()
